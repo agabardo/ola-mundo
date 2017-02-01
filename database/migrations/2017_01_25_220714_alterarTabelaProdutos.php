13dc02fc4ad9061415a1cb913672399c8aa24653
Ademir C. Gabardo <ademir.gabbardo@gmail.com>
2017-02-02 09:00:30 +1100

    Adicionada a migration para alteracao da tabela de produtos permitindo o uso de Soft Deleting

--- database/migrations/2017_01_25_220714_alterarTabelaProdutos.php
+++ database/migrations/2017_01_25_220714_alterarTabelaProdutos.php
@@ -1,8 +1,6 @@
 <?php
-
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-
 class AlterarTabelaProdutos extends Migration{
     public function up(){
       Schema::table('produtos', function ($table) {
