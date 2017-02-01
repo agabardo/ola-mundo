bee590befa564a27ceaf65765eb2693fbf6584ed
Ademir C. Gabardo <ademir.gabbardo@gmail.com>
2017-02-02 08:05:39 +1100

    Adicionados os arquivos traduzidos de login e recuperacao de senha

--- resources/views/auth/emails/password.blade.php
+++ resources/views/auth/emails/password.blade.php
@@ -1,1 +1,1 @@
-Click here to reset your password: <a href="{{ $link = url('password/reset', $token).'?email='.urlencode($user->getEmailForPasswordReset()) }}"> {{ $link }} </a>
+Clique aqui para redefinir sua senha: <a href="{{ $link = url('password/reset', $token).'?email='.urlencode($user->getEmailForPasswordReset()) }}"> {{ $link }} </a>
